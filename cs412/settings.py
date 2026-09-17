9f3de8391a1d3b67afa23b9de5231906f30bc78b
gabalingog <galingog@bu.edu>
2026-09-17 12:13:44 -0400

    initalized assignment 2 - restaurant

--- cs412/settings.py
+++ cs412/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'hw',
     'quotes',
     'formdata',
+    'restaurant',
 ]
 
 MIDDLEWARE = [
---
+++ restaurant/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+import random, time
+
+# Create your views here.
