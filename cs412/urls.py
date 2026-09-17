44b135d63d71dcb5b03a8d98f8c0d1c53ee53a53
gabalingog <galingog@bu.edu>
2026-09-17 00:15:16 -0400

    initialized formdata module 2

--- cs412/urls.py
+++ cs412/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', include('quotes.urls')), 
     path('hw/', include('hw.urls')),
     path('quotes/', include('quotes.urls')),
+    path('formdata/', include('formdata.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
