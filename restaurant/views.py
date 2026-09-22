e4a196137ce7bbcb50971746d4d73293a1a5382f
gabalingog <galingog@bu.edu>
2026-09-21 20:13:03 -0400

    all html and urls set up

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,3 +2,17 @@ from django.shortcuts import render
 import random, time
 
 # Create your views here.
+def main(request):
+    '''Show the form to the user'''
+    template_name = 'restaurant/main.html'
+    return render(request, template_name)
+
+def order(request):
+    '''Show the form to the user'''
+    template_name = 'restaurant/order.html'
+    return render(request, template_name)
+
+def confirmation(request):
+    '''Show the form to the user'''
+    template_name = 'restaurant/confirmation.html'
+    return render(request, template_name)
