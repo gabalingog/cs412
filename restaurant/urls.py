5ec1cb4af5605ce86e015beeccd34613a9747747
gabalingog <galingog@bu.edu>
2026-09-22 20:52:35 -0400

    fixed randomized specials

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,3 +1,7 @@
+# File: urls.py
+# Author: Gab Alingog (galingog@bu.edu), 09/12/2026
+# Description: Path names and configuration of the website
+
 from django.urls import path
 from django.conf import settings
 from . import views
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,44 +1,78 @@
+# File: views.py
+# Author: Gab Alingog (galingog@bu.edu), 09/22/2026
+# Description: Stores the menu data and the functions for each of the web pages
+
 from django.shortcuts import render
 import random, time
 
+# Main menu data
 prices = {
     'sinigang': ('Sinigang', 22.00),
     'tinola': ('Tinola', 20.00),
     'adobo': ('Adobo', 22.00),
     'nilaga': ('Nilaga', 20.00),
-    'mango': ('Mango Shake', 6.5),
+}
+
+# Specials menu data
+specials = {
+    'mango': ('Mango Shake', 6.50),
     'flan': ('Leche Flan', 5.00),
+    'sorbae': ('Sorbae', 3.50),
 }
 
 def main(request):
-    '''Show the form to the user'''
+    '''Show the restaurant welcome page to the user'''
     template_name = 'restaurant/main.html'
     return render(request, template_name)
 
 def order(request):
-    '''Show the form to the user'''
+    '''Show the menu order form that will be submitted'''
     template_name = 'restaurant/order.html'
-    return render(request, template_name)
+
+    # Randomized specials by indexing
+    key1 = random.choice(list(specials.keys()))
+    special1, price1 = specials[key1]
+
+    context = {
+        'key1': key1,
+        'special1': special1,
+        'price1': price1,
+    }
+    return render(request, template_name, context)
 
 def confirmation(request):
-    '''Show the form to the user'''
+    '''Show the confirmation receipt of a submitted order form'''
     template_name = 'restaurant/confirmation.html'
     
-    if request.POST: # if it exists
+    # Only if the order form was submitted
+    if request.POST:
         ordered = []
         total = 0.0
 
+        # Iterates through the name and price of each item in the menu, alongside the input
         for x, (name, price) in prices.items():
+            # If a valid menu item
+            if x in request.POST:
+                # Add the name and price to the list of what the user ordered
+                ordered.append((name, price))
+                # Add to the total price
+                total += price
+
+        # Another loop with same structure to account for specials
+        for x, (name, price) in specials.items():
             if x in request.POST:
                 ordered.append((name, price))
                 total += price
-            
+        
+        # Fetch the customer information
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         email = request.POST.get('email')
         instructions = request.POST.get('instructions')
 
-        time_later = random.randint(15, 45) * 60
+        # Random order time between 30 to 60 minutes converted to seconds
+        time_later = random.randint(30, 60) * 60
+        # Current time + the random computed time
         time_ready = time.ctime(time.time() + time_later)
 
         context = {
