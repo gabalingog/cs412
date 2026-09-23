336a2170f53619b5c0fe730e52d3fc37336ea60c
gabalingog <galingog@bu.edu>
2026-09-22 21:09:56 -0400

    finalized

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path(r'main', views.main, name='main'),
     path(r'order', views.order, name='order'),
     path(r'confirmation', views.confirmation, name='confirmation'),
